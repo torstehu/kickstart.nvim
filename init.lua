732e69c945d701901f12ddf01f74073f2cd926fb
Torstein Husebø <torstein@huseboe.net>
2026-06-27 07:36:19 +0200

    remove commented colorscheme

--- init.lua
+++ init.lua
@@ -388,16 +388,10 @@ do
   ---@diagnostic disable-next-line: missing-fields
   require("zenburn").setup {
   }
-  --require('tokyonight').setup {
-  --  styles = {
-  --    comments = { italic = false }, -- Disable italics in comments
-  --  },
-  --}
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --vim.cmd.colorscheme 'tokyonight-night'
   vim.cmd.colorscheme 'zenburn'
 
   -- Highlight todo, notes, etc in comments
